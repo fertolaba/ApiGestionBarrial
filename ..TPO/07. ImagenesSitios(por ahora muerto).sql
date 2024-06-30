d6150c7fbcf4f948181fe53b1f3ce84f37bd353c
Ramon Irala <rirala@garantizar.com.ar>
2024-06-30 19:32:59 -0300

    chau scripts (vuelven en siguiente commit)

--- ..TPO/07. ImagenesSitios(por ahora muerto).sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE imagenesSitios (
-    idImagenSitio INT PRIMARY KEY IDENTITY(1, 1),
-    idSitio INT NOT NULL,
-    url VARCHAR(MAX),
-
-    CONSTRAINT FK_imagenesSitios_sitios FOREIGN KEY (idSitio) REFERENCES sitios(idSitio)
-);
-
